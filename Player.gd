f7f334d3539edfc86e01c8e811167fef5a68d5aa
Francisco Stanfield <108752839+FreedominaStonyField@users.noreply.github.com>
2023-11-18 08:55:22 -0500

    removed commit

--- Player.gd
+++ Player.gd
@@ -72,7 +72,6 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
