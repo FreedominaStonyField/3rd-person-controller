97b801a394bd5d9c6aa62afa7acf2ae8e85c8e9e
Francisco Stanfield <108752839+FreedominaStonyField@users.noreply.github.com>
2023-11-18 08:47:13 -0500

    added basic state machine template

---
+++ addons/kenney_prototype_textures/orange/state_mashine.gd
@@ -0,0 +1,34 @@
+class_name State_Mashine
+
+extends Node
+
+
+@export var CURRENT_STATE : State
+
+var states :Dictionary = {}
+
+func _ready():
+	for child in get_children():
+		if child is State:
+			states[child.name] = child
+		else:
+			push_warning("State Mashine contians incompatible child node!")
+	CURRENT_STATE.enter()
+	
+func _process(delta):
+	CURRENT_STATE.update(delta)
+
+func _physics_process(delta):
+	CURRENT_STATE.physics_update(delta)
+
+
+func on_child_transition(new_state_name:StringName):
+	var new_state = states.get(new_state_name)
+	if new_state != null:
+		if new_state != CURRENT_STATE:
+			CURRENT_STATE.exit()
+			new_state.enter()
+			CURRENT_STATE = new_state
+	else:
+		push_warning("state does not exist!")
+	pass
---
+++ state_class.gd
@@ -0,0 +1,22 @@
+extends Node
+class_name State
+
+
+signal transition(new_state_name: StringName)
+
+
+func enter():
+	pass
+
+
+func exit():
+	pass
+
+
+func update(delta:float):
+	pass
+
+
+func physics_update(delta:float):
+	pass
+
