14a68e0676b0f0041d3f61d24bb2476e1d2bdd68
Francisco Stanfield <108752839+FreedominaStonyField@users.noreply.github.com>
2023-11-18 08:54:49 -0500

    updated inputs

--- Player.gd
+++ Player.gd
@@ -54,7 +54,7 @@ func _physics_process(delta):
 #			animation_player.play("t-pose")
 		
 	# Check if running
-	if Input.is_action_pressed("ui_home"):
+	if Input.is_action_pressed("sprint"):
 		running = true
 		SPEED = running_speed
 	else:
@@ -73,7 +73,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if direction:
